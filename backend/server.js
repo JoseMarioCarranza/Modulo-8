3d5aa18879c6d5c672f5b32be2dfce392d503eac
JoseMarioCarranza <imt_josecarranza@outlook.com>
2024-09-25 18:34:04 -0700

    Rutas, controladores, errorHandler y asyncHandler

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,15 @@ const { json } = require('body-parser')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/tareas', require('./routes/tareasRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Servidor iniciado en el puerto ${port}`))
